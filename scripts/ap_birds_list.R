f3968ef2fec8bd250058ebbb2bd01aacc4de7200
Juno <jpilson0701@gmail.com>
2026-02-01 16:53:04 -0500

    Streamline script

--- scripts/ap_birds_list.R
+++ scripts/ap_birds_list.R
@@ -1,53 +1,44 @@
 # install packages
 
-if (!require(terra)) install.packages("terra")
-if (!require(tidyterra)) install.packages("tidyterra")
-if (!require(readr)) install.packages("readr")
 if (!require(dplyr)) install.packages("dplyr")
+if (!require(readr)) install.packages("readr")
 if (!require(tidyverse)) install.packages("tidyverse")
 if (!require(vroom)) install.packages("vroom")
 
-
 #load packages
-          
-library(terra)     
-library(tidyterra)       
+
+library(dplyr)          
 library(readr)       
-library(dplyr)   
 library(tidyverse) 
 library(vroom)
 
 # load data
 
-ap_birds <- vroom("C:/Users/cpils/Documents/EDS Cert/Capstone/Data/Processed Data/GBIF Bird Data/Bird Observations shp/ap_bird_obs.csv")
-iucn <- vroom("C:/Users/cpils/Documents/EDS Cert/Capstone/Data/Raw Data/IUCN Bird List/assessments.csv")
+ap_birds <- vroom("C:/Users/cpils/Documents/EDS Cert/Capstone/Final Data Sources/AP Bird Occurences - csv (processed)/apbirds_obs.csv")
+bird_life <- vroom("C:/Users/cpils/Documents/EDS Cert/Capstone/Final Data Sources/BirdLife Species List (raw)/species-filter-results.csv")
 
-# generate a list of AP bird species
+# examine data for number of occurrences for each species 
 
-ap_birds_str <- unique(ap_birds$species)
+occ_tab <- table(ap_birds$species)
+occ_split <- write.table(occ_tab, file = "", append = FALSE, quote = TRUE, sep = " ",
+            eol = "/n", na = "NA", dec = ".", row.names = TRUE,
+            col.names = TRUE, qmethod = c("escape", "double"),
+            fileEncoding = "")
+occ_df <- data.frame(occ_tab)
+occ_df <- rename(occ_df, species = Var1)
 
-# turn string into a list
+# remove species with only one occurrence
 
-ap_birds_list <- as.list(strsplit(ap_birds_str, " "))
+ap_list <- subset(occ_df, Freq>1)
 
-# turn list into a df
+# add BirdLife data
 
-ap_birds_df <- do.call(rbind.data.frame, ap_birds_list)
+birdlife_slim <- bird_life %>%
+  select("Scientific name", "Family", "RL Category", "Migratory status", "Current population trend")
 
-ap_birds_df$species <- paste(ap_birds_df$c..Passer....Corvus....Ploceus....Corvus....Lamprotornis....Ammomanopsis..., ap_birds_df$c..melanurus....albus....xanthops....capensis....nitens....grayi...)
-
-birds_ap <- ap_birds_df %>%
-  select(species)
-
-# add iucn & habitat info
-
-iucn_slim <- iucn %>%
-  select(scientificName, habitat, redlistCategory)
-
-birds_join <- left_join(birds_ap, iucn_slim, by = c("species" = "scientificName"))
+birds_join <- left_join(ap_list, birdlife_slim, by = c("species" = "Scientific name"))
 
 # export list
 
-write_csv(birds_join, "C:/Users/cpils/Documents/EDS Cert/Capstone/Data/Processed Data/AP Bird list/ap_bird_list.csv")
-
+write_csv(birds_join, "C:/Users/cpils/Documents/EDS Cert/Capstone/Final Data Sources/AP Birds List (processed)/ap_bird_list.csv")
 
